d79574127ca394852e1a56804bdc9160ae2a6e11
romainbernasconi1 <romainbernasconi1@gmail.com>
2026-03-27 17:12:00 +0100

    ajout requête SQL pour lon/lat

--- apache-php/src/index.php
+++ apache-php/src/index.php
@@ -15,7 +15,27 @@ Flight::set('geobase', $link);
 Flight::get('geobase');
 
 Flight::route('/test-db', function () {
-    $sql = "SELECT nom, insee FROM communes";
+// Paramètres de l'URL (on fait ici la méthode GET)
+    $recherche = $_GET['nom'] ?? '';
+    $mode = $_GET['mode'] ?? 'contient';
+
+// ajout de l'opérateur LIKE pour commence par, finit par et contient
+if ($mode === 'commence par') {
+    $rechercheSQL = $recherche . '%';
+} elseif ($mode === 'finit par') {
+    $rechercheSQL = '%' . $rercherche;
+} else {
+    $rechercheSQL = '%' . $recherche . '%';
+}
+
+// requête SQL principale pour extraire les longitudes et latitudes
+    $sql = "SELECT nom, insee, 
+            ST_X(ST_GeomFromText(ST_AsText(ST_Centroid(geometry)),4326)) AS lon,
+            ST_Y(ST_GeomFromText(ST_AsText(ST_Centroid(geometry)),4326)) AS lat
+            FROM communes 
+            WHERE nom LIKE '$rechercheSQL'
+            AND geometry IS NOT NULL AND ST_IsValid(geometry) = 1";
+
     $query = mysqli_query(Flight::get('geobase'), $sql);
     $results = mysqli_fetch_all($query, MYSQLI_ASSOC);
     Flight::json($results);
